07a1a3424c077d1520cf7382e1349d8d44cf0367
humphreyotieno1 <humphreyotieno04@gmail.com>
2026-07-10 12:31:42 +0300

    feat: update Content Security Policy to include tawk.to domains for enhanced security and support integration

--- next.config.mjs
+++ next.config.mjs
@@ -25,12 +25,12 @@ const nextConfig = {
   async headers() {
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googletagmanager.com *.google-analytics.com *.facebook.net *.facebook.com *.cloudinary.com *.vercel-scripts.com *.google.com;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googletagmanager.com *.google-analytics.com *.facebook.net *.facebook.com *.cloudinary.com *.vercel-scripts.com *.google.com *.tawk.to;
       style-src 'self' 'unsafe-inline' fonts.googleapis.com;
-      img-src 'self' blob: data: *.cloudinary.com *.googletagmanager.com *.google-analytics.com *.facebook.com *.facebook.net *.google.com *.gstatic.com *.googleapis.com;
+      img-src 'self' blob: data: *.cloudinary.com *.googletagmanager.com *.google-analytics.com *.facebook.com *.facebook.net *.google.com *.gstatic.com *.googleapis.com *.tawk.to *.tawk.link;
       font-src 'self' data: fonts.gstatic.com;
-      connect-src 'self' *.googletagmanager.com *.google-analytics.com *.analytics.google.com *.facebook.com *.facebook.net *.cloudinary.com *.google.com;
-      frame-src 'self' *.facebook.com *.facebook.net *.google.com;
+      connect-src 'self' *.googletagmanager.com *.google-analytics.com *.analytics.google.com *.facebook.com *.facebook.net *.cloudinary.com *.google.com *.tawk.to *.tawk.link https://*.tawk.to wss://*.tawk.to;
+      frame-src 'self' *.facebook.com *.facebook.net *.google.com *.tawk.to *.tawk.link;
       base-uri 'self';
       form-action 'self';
       frame-ancestors 'self';
